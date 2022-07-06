c991febf77dceea18790640895fe5722c2d090a0
Yuriy Putsan <putsan57@gmail.com>
2022-07-06 23:06:41 +0300

    next part updates

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,9 +8,9 @@ const toggleBurgerMenu = () => {
   const navMenu = document.querySelector(".navigation__menu");
   const navContainer = document.querySelector(".navigation");
 
-  const toggleMenu = () => navMenu.classList.toggle(ACTIVE_MENU_CLASS_NAME);
+  const toggleMenu = () => toggleClass(navMenu, ACTIVE_MENU_CLASS_NAME);
   const hideMenu = () =>
-  navMenu.classList.contains(ACTIVE_MENU_CLASS_NAME) && toggleMenu();
+    navMenu.classList.contains(ACTIVE_MENU_CLASS_NAME) && toggleMenu();
 
   navContainer.addEventListener("click", ({ target }) => {
     const isTargetBtn = target.className === TARGET_BTN_CLASS_NAME;
@@ -25,10 +25,45 @@ const toggleBurgerMenu = () => {
 
 toggleBurgerMenu();
 
+const togglePostVisibility = () => {
+  const VISIBLE_POST_CN = "post__content--visible";
+  const BUTTON_TO_TOP_CN = "button--top";
+  const BUTTON_TEXT = "Читати далі";
+  const BUTTON_TEXT_TOP = "Сховати";
+
+  const posts = document.querySelector("#posts");
+
+  posts.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const target = e.target;
+    const button = target.closest(".post__button");
+
+    if (button) {
+      const targetPost = target.closest(".post");
+      const postContent = targetPost.querySelector("main");
+      const buttonSpan = button.querySelector(".button__text");
+
+      toggleClass(postContent, VISIBLE_POST_CN);
+      toggleClass(button, BUTTON_TO_TOP_CN);
+
+      (buttonSpan.innerText.trim() === BUTTON_TEXT &&
+        (buttonSpan.innerHTML = BUTTON_TEXT_TOP)) ||
+        (buttonSpan.innerHTML = BUTTON_TEXT);
+    }
+  });
+};
+
+togglePostVisibility();
+
+function toggleClass(elem, className) {
+  elem.classList.toggle(className);
+}
+
 function onClickOutside(element, func) {
   const outsideClickListener = (event) => {
     if (!element.contains(event.target)) {
-    // if (event.target.closest(selector) === null) {
+      // if (event.target.closest(selector) === null) {
       // or use: event.target.closest(selector) === null
       func();
       removeClickListener();
