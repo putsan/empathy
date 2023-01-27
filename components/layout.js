d4a0f8e6ff58f3626b639cbf7877e691ed1fabe4
Yuriy Putsan <putsan57@gmail.com>
2023-01-27 22:56:32 +0200

    move some header elements

--- components/layout.js
+++ components/layout.js
@@ -1,32 +1,76 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = 'Yuriy Namir';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle =
+  "Ваш психолог у Львові: консультації, тренінги, коучинг ❘ Підгородний";
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        {/* <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
-        />
-        <meta
+        /> */}
+        {/* <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle,
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary_large_image" /> */}
       </Head>
-      <header className={styles.header}>
-        {home ? (
+      <header id="header" className={styles.header}>
+        <nav class="navigation">
+          <div class="navigation__left">
+            <img src="./images/menu.svg" class="navigation__menu-btn" alt="" />
+            <div class="navigation__line"></div>
+            <img
+              src="./images/logo.png"
+              alt="Емпатія"
+              class="navigation__logo"
+            />
+          </div>
+
+          <div class="navigation__menu">
+            <ul class="navigation__list">
+              <li class="navigation__item">
+                <a href="#services" class="navigation__link">
+                  Послуги
+                </a>
+              </li>
+              <li class="navigation__item">
+                <a href="#about" class="navigation__link">
+                  Про мене
+                </a>
+              </li>
+              <li class="navigation__item">
+                <a href="#prices" class="navigation__link">
+                  Ціни
+                </a>
+              </li>
+              <li class="navigation__item">
+                <a href="#posts" class="navigation__link">
+                  Статті
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <button class="navigation__button">
+            <a href="#contact-form" class="navigation__btn-link">
+              Записатись
+            </a>
+          </button>
+        </nav>
+        {/* {home ? (
           <>
             <Image
               priority
@@ -56,7 +100,7 @@ export default function Layout({ children, home }) {
               </Link>
             </h2>
           </>
-        )}
+        )} */}
       </header>
       <main>{children}</main>
       {!home && (
